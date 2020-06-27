126b86b519f5f36b3d70d4770a8c2af953101688
Brianna Workman <brianna.workman@hotmail.com>
2020-06-27 12:57:21 -0400

    styles languages card

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -10,6 +10,36 @@ function UserInfo(props) {
   // const [user, setUser] = useState({});
   // const [langData, setLangData] = useState(null);
   // const [repoData, setRepoData] = useState(null);
+  // const [topRepos, setTopRepos] = useState([]);
+  // const [sortType, setSortType] = useState('stars');
+  // const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  // const getTopRepos = (type) => {
+  //   const LIMIT = 8;
+  //   const map = {
+  //     stars: 'stargazers_count',
+  //     forks: 'forks_count',
+  //     size: 'size',
+  //   };
+  //   const sortProperty = map[type];
+  //   const sorted = repoData
+  //     ? repoData
+  //         .filter((repo) => !repo.fork)
+  //         .sort((a, b) => b[sortProperty] - a[sortProperty])
+  //         .slice(0, LIMIT)
+  //     : console.log('loading...');
+
+  //   setTopRepos(sorted);
+  // };
+
+  // const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+  // const changeRepoSort = (sortType) => {
+  //   setSortType(sortType);
+  //   toggleDropdown();
+  // };
+
+  // const sortTypes = ['stars', 'forks', 'size'];
 
   // const getLangData = () => {
   //   const me = new GhPolyglot(`${username}`);
@@ -46,7 +76,19 @@ function UserInfo(props) {
 
   //   getLangData();
   //   getRepoData();
+
+  //   console.log(repoData);
   // }, []);
+
+  // useEffect(() => {
+  //   if (repoData) {
+  //     console.log(repoData);
+  //     getTopRepos();
+  //     console.log(topRepos);
+  //   }
+  // }, []);
+
+  // useEffect(() => getTopRepos(sortType), [sortType]);
 
   var langData = [
     { label: 'Python', value: 11, color: '#3572A5' },
@@ -75,67 +117,81 @@ function UserInfo(props) {
     : null;
 
   return (
-    <div className='user-container'>
-      <img src={user.avatar_url} className='profile-pic' />
-      <h1>{user.name}</h1>
-      <a
-        href={`https://github.com/${user.login}`}
-        target='_blank'
-        rel='noopener noreferrer'
-        className='username-link'
-      >
-        @{user.login}
-      </a>
-      <div className='user-info'>
-        <div className='info'>
-          <img src={mapMarker} alt='Location Marker Icon' className='icons' />
-          <p>{user.location}</p>
-        </div>
-        <div className='info'>
-          <img src={calendar} alt='Calendar Icon' className='icons' />
-          <p>
-            Joined{' '}
-            {new Date(user.created_at).toLocaleDateString('en-US', {
-              month: 'long',
-              day: 'numeric',
-              year: 'numeric',
-            })}
-          </p>
-        </div>
-      </div>
-      <div className='github-stats'>
-        <div className='stats'>
-          <h3>{user.public_repos}</h3>
-          <p>REPOSITORIES</p>
-        </div>
-        <div className='stats'>
-          <h3>{user.followers}</h3>
-          <p>FOLLOWERS</p>
-        </div>
-        <div className='stats'>
-          <h3>{user.following}</h3>
-          <p>FOLLOWING</p>
-        </div>
-      </div>
-      <div className='chart'>
-        <PieChart width={550} height={400} className='pie-chart'>
-          <Pie
-            data={langData ? langData : []}
-            labelLine={true}
-            fill='#8884d8'
-            dataKey='value'
-            label={(entry) => entry.label}
+    <div className='container'>
+      <div className='main-container'>
+        <div className='user-container'>
+          <img
+            src={user.avatar_url}
+            className='profile-pic'
+            alt={`${username} profile picture`}
+          />
+          <h1>{user.name}</h1>
+          <a
+            href={`https://github.com/${user.login}`}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='username-link'
           >
-            {langData
-              ? langData.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))
-              : []}
-          </Pie>
-        </PieChart>
+            @{user.login}
+          </a>
+          <div className='user-info'>
+            <div className='info'>
+              <img
+                src={mapMarker}
+                alt='Location Marker Icon'
+                className='icons'
+              />
+              <p>{user.location}</p>
+            </div>
+            <div className='info'>
+              <img src={calendar} alt='Calendar Icon' className='icons' />
+              <p>
+                Joined{' '}
+                {new Date(user.created_at).toLocaleDateString('en-US', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })}
+              </p>
+            </div>
+          </div>
+          <div className='github-stats'>
+            <div className='stats'>
+              <h3>{user.public_repos}</h3>
+              <p>REPOSITORIES</p>
+            </div>
+            <div className='stats'>
+              <h3>{user.followers}</h3>
+              <p>FOLLOWERS</p>
+            </div>
+            <div className='stats'>
+              <h3>{user.following}</h3>
+              <p>FOLLOWING</p>
+            </div>
+          </div>
+        </div>
+        <div className='chart'>
+          <h1>Top Languages</h1>
+          <PieChart width={400} height={300} className='pie-chart'>
+            <Pie
+              data={langData ? langData : []}
+              labelLine={true}
+              fill='#8884d8'
+              dataKey='value'
+              label={(entry) => entry.label}
+            >
+              {langData
+                ? langData.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))
+                : []}
+            </Pie>
+          </PieChart>
+        </div>
+        {/* <button onClick={() => topRepos()}>Repos</button> */}
       </div>
     </div>
   );
