14f252fc403df582d16849f3c15f6fd87c67ee7a
Brianna Workman <brianna.workman@hotmail.com>
2020-06-28 22:01:22 -0400

    adds star and fork icons, styles repo cards

--- src/components/TopRepos.js
+++ src/components/TopRepos.js
@@ -1,60 +1,130 @@
 import React, { useEffect, useState } from 'react';
+import star from '../assets/star.png';
+import fork from '../assets/fork.png';
 
 function TopRepos(repoData) {
-  const [topRepos, setTopRepos] = useState([]);
-  const [sortType, setSortType] = useState('stars');
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  // const [topRepos, setTopRepos] = useState([]);
+  // const [sortType, setSortType] = useState('stars');
+  // const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  // console.log(repoData.repoData);
+  // // console.log(repoData.repoData);
 
-  const getTopRepos = (type) => {
-    const LIMIT = 6;
-    const map = {
-      stars: 'stargazers_count',
-      forks: 'forks_count',
-      size: 'size',
-    };
+  // const getTopRepos = (type) => {
+  //   const LIMIT = 8;
+  //   const map = {
+  //     stars: 'stargazers_count',
+  //     forks: 'forks_count',
+  //     size: 'size',
+  //   };
 
-    const sortProperty = map[type];
-    const sorted = repoData.repoData
-      ? repoData.repoData
-          .filter((repo) => !repo.fork)
-          .sort((a, b) => b[sortProperty] - a[sortProperty])
-          .slice(0, LIMIT)
-      : console.log('Sorting Repos...');
-    setTopRepos(sorted);
-  };
+  //   const sortProperty = map[type];
+  //   const sorted = repoData.repoData
+  //     ? repoData.repoData
+  //         .filter((repo) => !repo.fork)
+  //         .sort((a, b) => b[sortProperty] - a[sortProperty])
+  //         .slice(0, LIMIT)
+  //     : console.log('Sorting Repos...');
+  //   setTopRepos(sorted);
+  // };
 
-  const sortTypes = ['stars', 'forks', 'size'];
+  // const sortTypes = ['stars', 'forks', 'size'];
 
-  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+  // const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
 
-  const changeRepoSort = () => {
-    setSortType(sortType);
-    toggleDropdown();
-  };
+  // const changeRepoSort = () => {
+  //   setSortType(sortType);
+  //   toggleDropdown();
+  // };
 
-  useEffect(() => {
-    getTopRepos();
-  }, []);
+  // useEffect(() => {
+  //   getTopRepos();
+  // }, []);
+
+  const topRepos = [
+    {
+      name: 'Algorithms',
+      language: 'Python',
+      size: 38,
+      stargazers_count: 0,
+      forks: 0,
+    },
+    {
+      name: 'budgety',
+      language: 'JavaScript',
+      size: 1230,
+      stargazers_count: 0,
+      forks: 0,
+    },
+    {
+      name: 'Computer-Architecture',
+      language: 'Python',
+      size: 53,
+      stargazers_count: 0,
+      forks: 0,
+    },
+    {
+      name: 'covid-19',
+      language: 'JavaScript',
+      size: 687,
+      stargazers_count: 0,
+      forks: 0,
+    },
+    {
+      name: 'Data-Structures',
+      language: 'Python',
+      size: 375,
+      stargazers_count: 0,
+      forks: 0,
+    },
+    {
+      name: 'github-profile',
+      language: 'JavaScript',
+      size: 881,
+      stargazers_count: 0,
+      forks: 0,
+    },
+    {
+      name: 'Data-Structures',
+      language: 'Python',
+      size: 375,
+      stargazers_count: 0,
+      forks: 0,
+    },
+    {
+      name: 'github-profile',
+      language: 'JavaScript',
+      size: 881,
+      stargazers_count: 0,
+      forks: 0,
+    },
+  ];
 
   console.log(topRepos);
 
   return (
-    <div>
-      {topRepos.map((repos) => {
-        return (
-          <div className='top-repos'>
-            <h2>{repos.name}</h2>
-            <div className='repo-data'>
-              <p>{repos.language}</p>
-              <p>{repos.size}</p>
-              <p>{repos.stargazers_count}</p>
-              <p>{repos.forks}</p>
+    <div className='repo-container'>
+      <h1>Top Repos</h1>
+      <div className='repo-cards'>
+        {topRepos.map((repos) => {
+          return (
+            <div className='top-repos'>
+              <h2>{repos.name}</h2>
+              <div className='repo-data'>
+                <p>{repos.language}</p>
+                <p>{repos.size} MB</p>
+                <div className='icon-container'>
+                  <img src={star} alt='star' className='star-icon' />
+                  <p>{repos.stargazers_count}</p>
+                </div>
+                <div className='icon-container'>
+                  <img src={fork} alt='fork' className='fork-icon' />
+                  <p>{repos.forks}</p>
+                </div>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -8,80 +8,68 @@ import TopRepos from './TopRepos';
 
 function UserInfo(props) {
   let username = props.location.state.username;
-  const [user, setUser] = useState({});
-  const [langData, setLangData] = useState(null);
-  const [repoData, setRepoData] = useState(null);
+  // const [user, setUser] = useState({});
+  // const [langData, setLangData] = useState(null);
+  // const [repoData, setRepoData] = useState(null);
 
-  const getLangData = () => {
-    const me = new GhPolyglot(`${username}`);
-    me.userStats((err, stats) => {
-      if (err) {
-        console.error('Error:', err);
-      }
-      setLangData(stats);
-    });
-  };
+  // const getLangData = () => {
+  //   const me = new GhPolyglot(`${username}`);
+  //   me.userStats((err, stats) => {
+  //     if (err) {
+  //       console.error('Error:', err);
+  //     }
+  //     setLangData(stats);
+  //   });
+  // };
 
-  const getRepoData = () => {
-    axios
-      .get(`https://api.github.com/users/${username}/repos?per_page=100`)
-      .then((response) => {
-        response.data
-          ? setRepoData(response.data)
-          : console.log('loading repos');
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  };
+  // const getRepoData = () => {
+  //   axios
+  //     .get(`https://api.github.com/users/${username}/repos?per_page=100`)
+  //     .then((response) => {
+  //       response.data
+  //         ? setRepoData(response.data)
+  //         : console.log('loading repos');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error);
+  //     });
+  // };
 
-  useEffect(() => {
-    getLangData();
-    getRepoData();
-
-    axios
-      .get(`https://api.github.com/users/${username}`)
-      .then((response) => {
-        // console.log(response);
-        setUser(response.data);
-      })
-
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  // console.log('TOP REPOS:', topRepos);
   // useEffect(() => {
-  //   // repoData
-  //   //   ? console.log('REPO DATA:', repoData)
-  //   //   : console.log('...loading repo data');
-  //   getTopRepos();
+  //   getLangData();
+  //   getRepoData();
+
+  //   axios
+  //     .get(`https://api.github.com/users/${username}`)
+  //     .then((response) => {
+  //       // console.log(response);
+  //       setUser(response.data);
+  //     })
+
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
   // }, []);
 
-  // console.log('REPO DATA:', repoData);
+  var langData = [
+    { label: 'Python', value: 11, color: '#3572A5' },
+    { label: 'JavaScript', value: 10, color: '#f1e05a' },
+    { label: 'Others', value: 4, color: '#ccc' },
+    { label: 'HTML', value: 1, color: '#e34c26' },
+    { label: 'CSS', value: 1, color: '#563d7c' },
+  ];
 
-  // useEffect(() => getTopRepos(sortType), [sortType]);
-
-  // var langData = [
-  //   { label: 'Python', value: 11, color: '#3572A5' },
-  //   { label: 'JavaScript', value: 10, color: '#f1e05a' },
-  //   { label: 'Others', value: 4, color: '#ccc' },
-  //   { label: 'HTML', value: 1, color: '#e34c26' },
-  //   { label: 'CSS', value: 1, color: '#563d7c' },
-  // ];
-
-  // var user = {
-  //   avatar_url:
-  //     'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2978&q=80',
-  //   name: 'Brianna Workman',
-  //   login: `${username}`,
-  //   location: 'Auburn Hills, MI',
-  //   created_at: '2019-06-13T19:23:02Z',
-  //   public_repos: '108',
-  //   followers: '19',
-  //   following: '25',
-  // };
+  var user = {
+    avatar_url:
+      'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2978&q=80',
+    name: 'Brianna Workman',
+    login: `${username}`,
+    location: 'Auburn Hills, MI',
+    created_at: '2019-06-13T19:23:02Z',
+    public_repos: '108',
+    followers: '19',
+    following: '25',
+  };
 
   var COLORS = langData
     ? langData.map((colors) => {
@@ -143,29 +131,31 @@ function UserInfo(props) {
             </div>
           </div>
         </div>
-        <div className='chart'>
-          <h1>Top Languages</h1>
-          <PieChart width={400} height={300} className='pie-chart'>
-            <Pie
-              data={langData ? langData : []}
-              labelLine={true}
-              fill='#8884d8'
-              dataKey='value'
-              label={(entry) => entry.label}
-            >
-              {langData
-                ? langData.map((entry, index) => (
-                    <Cell
-                      key={`cell-${index}`}
-                      fill={COLORS[index % COLORS.length]}
-                    />
-                  ))
-                : []}
-            </Pie>
-          </PieChart>
+        <div className='info-container'>
+          <div className='chart'>
+            <h1>Top Languages</h1>
+            <PieChart width={400} height={300} className='pie-chart'>
+              <Pie
+                data={langData ? langData : []}
+                labelLine={true}
+                fill='#8884d8'
+                dataKey='value'
+                label={(entry) => entry.label}
+              >
+                {langData
+                  ? langData.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))
+                  : []}
+              </Pie>
+            </PieChart>
+          </div>
+          {/* {repoData ? <TopRepos repoData={repoData} /> : null} */}
+          <TopRepos />
         </div>
-        {repoData ? <TopRepos repoData={repoData} /> : null}
-        {/* <button onClick={() => getTopRepos()}>Repos</button> */}
       </div>
     </div>
   );
