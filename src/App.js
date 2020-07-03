9c31d6dbda016808c4972f6d07afb00288cefda0
Brianna Workman <brianna.workman@hotmail.com>
2020-07-03 00:12:03 -0400

    adds github corner icon and animations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import GithubCorner from './components/GithubCorner';
 import UserData from './components/UserData';
 import UserName from './components/UserName';
 import Footer from './components/Footer';
@@ -9,6 +10,7 @@ import { Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className='App'>
+      <GithubCorner />
       <Switch>
         <Route exact path='/' component={UserName} />
         <Route exact path='/profile/:id' component={UserData} />
