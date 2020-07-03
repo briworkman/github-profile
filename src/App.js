bf4f774650e23661160cad01e3c5d4cb638fc9a8
Brianna Workman <brianna.workman@hotmail.com>
2020-07-02 23:08:21 -0400

    adds footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import UserData from './components/UserData';
 import UserName from './components/UserName';
+import Footer from './components/Footer';
 
 import { Route, Switch } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/' component={UserName} />
         <Route exact path='/profile/:id' component={UserData} />
       </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>
+        Designed & Built by{' '}
+        <a
+          href='https://brianna-workman.netlify.app/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Brianna Workman
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
