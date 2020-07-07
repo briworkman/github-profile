a1c4bf43331c2a10a5fe85333d77832ffa346f55
Brianna Workman <brianna.workman@hotmail.com>
2020-07-07 16:51:52 -0400

    changes pie chart height

--- src/components/TopLanguages.js
+++ src/components/TopLanguages.js
@@ -6,7 +6,7 @@ function TopLanguages(props) {
     <div className='chart-container'>
       <div className='chart'>
         <h1>Top Languages</h1>
-        <PieChart width={400} height={300} className='pie-chart'>
+        <PieChart width={400} height={350} className='pie-chart'>
           <Pie
             data={props.languageData ? props.languageData : []}
             labelLine={true}
